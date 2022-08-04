c7bcee251fb88d46ec8cdfed8586fc34dc6c32a2
Gagan Bhatia <gbhatia880@gmail.com>
2022-08-04 16:39:59 -0700

    fix bug in cnn_bilstm.py

--- src/models/components/cnn_bilstm.py
+++ src/models/components/cnn_bilstm.py
@@ -5,8 +5,8 @@ class CNN_BiLSTM(nn.Module):
         super().__init__()
 
         self.cnn = nn.Sequential(
-            nn.Conv2d(1, 8, kernel_size=(5,9), stride=1, padding=1,filters=8),
-            nn.Conv2d(8, 8, kernel_size=(5,7), stride=1, padding=1,filters=8),
+            nn.Conv2d(1, 8, kernel_size=(5,9), stride=1, padding=1),
+            nn.Conv2d(8, 8, kernel_size=(5,7), stride=1, padding=1),
             nn.LSTM(8, 32, bidirectional=True),
             nn.LSTM(32*2, 32, bidirectional=True),
             nn.LSTM(32*2, 32, bidirectional=True),
